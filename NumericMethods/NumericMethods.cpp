c4c6d405a261fbc2c7255e660f095316fba15004
envi <envi@DESKTOP-8L586VE>
2024-12-21 15:00:26 +0300

    ttt

--- NumericMethods/NumericMethods.cpp
+++ NumericMethods/NumericMethods.cpp
@@ -4,15 +4,19 @@
 #include <iostream>
 #include "FredgholmEquation.h"
 #include "XLSXWorker.h"
+#include "VolterEquation.h"
 int main()
 {
     Fredgholm* fr = new Fredgholm;
+    Volter* vl = new Volter();
     xlsx_worker* excel = new xlsx_worker();
     fr->upper = 3;
     fr->lower = 1;
     fr->step = 10;
     auto sol = fredgholm_solver_by_trapezium(fr);
-    excel->write_solution_for_fredgholm_to_excel("popa.xls", fr->weights, sol);
+    //excel->write_solution_for_fredgholm_to_excel("popa.xls", fr->weights, sol);
+    vl->solve_fde_by_runge_kutta(0, 0, 0, 5.0, 0.25);
+    delete vl;
     delete excel;
     delete fr;
 
---
+++ NumericMethods/VolterEquation.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+#include "stdafx.h"
+class Volter {
+	
+public:
+	double f1(double t, double y1, double y2) {
+		return y2; 
+	}
+
+	double f2(double t, double y1, double y2) {
+		return -4 * y1 + 3 * cos(t);
+	}
+
+	void solve_fde_by_runge_kutta(double t0, double y1_0, double y2_0, double t_end, double h) {
+		
+       std::vector<double> t_values, x_values;
+        double t = t0, y1 = y1_0, y2 = y2_0;
+
+
+        while (t <= t_end) {
+            // Сохраняем текущие значения
+            t_values.push_back(t);
+            x_values.push_back(y2);  // x(t) = y'(t)
+
+            // Записываем данные в файл
+            std::cout << " " << t << " " << y2 << std::endl;
+
+            // Коэффициенты Рунге-Кутты для y1
+            double k1_1 = h * f1(t, y1, y2);
+            double k2_1 = h * f1(t + h / 2, y1 + k1_1 / 2, y2);
+            double k3_1 = h * f1(t + h / 2, y1 + k2_1 / 2, y2);
+            double k4_1 = h * f1(t + h, y1 + k3_1, y2);
+
+            // Коэффициенты Рунге-Кутты для y2
+            double k1_2 = h * f2(t, y1, y2);
+            double k2_2 = h * f2(t + h / 2, y1, y2 + k1_2 / 2);
+            double k3_2 = h * f2(t + h / 2, y1, y2 + k2_2 / 2);
+            double k4_2 = h * f2(t + h, y1, y2 + k3_2);
+
+            // Обновление y1 и y2
+            y1 += (k1_1 + 2 * k2_1 + 2 * k3_1 + k4_1) / 6;
+            y2 += (k1_2 + 2 * k2_2 + 2 * k3_2 + k4_2) / 6;
+
+            // Обновление времени
+            t += h;
+        }
+
+        
+        
+
+	}
+
+};
